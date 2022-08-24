e3bf082307d788ec215121085411b2e824b04280
Mason Thieman <masonthieman@gmail.com>
2022-08-24 15:43:36 -0500

    Added feature that allows user to play again

--- Program.cs
+++ Program.cs
@@ -46,37 +46,46 @@ namespace Quest
             Console.Write("What is your name? ");
             string adventurerName = Console.ReadLine();
             Adventurer theAdventurer = new Adventurer(adventurerName);
+            
+            while (true)
+            {
+                // A list of challenges for the Adventurer to complete
+                // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
+                List<Challenge> challenges = new List<Challenge>()
+                {
+                    twoPlusTwo,
+                    theAnswer,
+                    whatSecond,
+                    guessRandom,
+                    favoriteBeatle
+                };
 
-            // A list of challenges for the Adventurer to complete
-            // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
-            List<Challenge> challenges = new List<Challenge>()
-            {
-                twoPlusTwo,
-                theAnswer,
-                whatSecond,
-                guessRandom,
-                favoriteBeatle
-            };
+                // Loop through all the challenges and subject the Adventurer to them
+                foreach (Challenge challenge in challenges)
+                {
+                    challenge.RunChallenge(theAdventurer);
+                }
 
-            // Loop through all the challenges and subject the Adventurer to them
-            foreach (Challenge challenge in challenges)
-            {
-                challenge.RunChallenge(theAdventurer);
-            }
-
-            // This code examines how Awesome the Adventurer is after completing the challenges
-            // And praises or humiliates them accordingly
-            if (theAdventurer.Awesomeness >= maxAwesomeness)
-            {
-                Console.WriteLine("YOU DID IT! You are truly awesome!");
-            }
-            else if (theAdventurer.Awesomeness <= minAwesomeness)
-            {
-                Console.WriteLine("Get out of my sight. Your lack of awesomeness offends me!");
-            }
-            else
-            {
-                Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
+                // This code examines how Awesome the Adventurer is after completing the challenges
+                // And praises or humiliates them accordingly
+                if (theAdventurer.Awesomeness >= maxAwesomeness)
+                {
+                    Console.WriteLine("YOU DID IT! You are truly awesome!");
+                }
+                else if (theAdventurer.Awesomeness <= minAwesomeness)
+                {
+                    Console.WriteLine("Get out of my sight. Your lack of awesomeness offends me!");
+                }
+                else
+                {
+                    Console.WriteLine("I guess you did...ok? ...sorta. Still, you should get out of my sight.");
+                }
+                Console.Write("Would you like to play again? ");
+                string playAgain = Console.ReadLine();
+                if (playAgain.ToLower() != "yes")
+                {
+                    break;
+                }
             }
         }
     }
