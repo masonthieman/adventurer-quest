3cb0b2fe6ebd948322253e976206c758601615d2
Mason Thieman <masonthieman@gmail.com>
2022-08-24 15:07:32 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace adventure_quest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
